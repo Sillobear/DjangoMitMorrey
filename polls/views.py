731ce3bbd2bd085106c4e1cf5e4238bd5997d471
Sillobear <sbranden@uni-muenster.de>
2022-06-08 11:08:20 +0200

    .

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,10 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.views import generic
+
+from .models import Question, Choice
+
 
 # Create your views here.
 def index(request):
